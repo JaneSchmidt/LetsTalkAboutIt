5974fcd0bc09747cd6ceb8bf79a8da9b482f81eb
janeschmidt <jane.schmidt123@gmail.com>
2024-05-12 23:03:03 -0400

    Home page CRUD operations and styling

--- htdocs/Models/homeModel.php
+++ htdocs/Models/homeModel.php
@@ -3,7 +3,21 @@
 
 class HomeModel{
 
+    public function getArticles(){
+        $query = "SELECT * FROM aritcles;";
+        
+        $connection = Connection::getConnection();
+        
+        $result = mysqli_query($connection, $query);
+        var_dump($result);
 
+        if(!$result){
+            echo "Query failed";
+        } else {
+            header('location: /login');
+        }
+
+    }
 
 
     
--- htdocs/Views/HomeView.php
+++ htdocs/Views/HomeView.php
@@ -3,14 +3,21 @@
   <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../Styling/homeStyle.css">
-  </head>
-  <body>
+</head>
+  <body class="container">
     <?php 
       include __DIR__ . "/../Attributes/navBar.php";
+      include __DIR__ . "/../Models/homeModel.php";
+
+      $model = new homeModel();
     ?>
 
-<div class="container">
+    <h1 class="header">
+      Let's Talk About It
+    </h1>
+
     <div class="blog">
+
       <div class="blog-post">
 
         <header class="subject">
@@ -18,14 +25,20 @@
         </header>
 
         <div class="content">
-          <p>Content of blog one hehre herh akjfne iwekjbfka fajegn iwejrlkasFJIU RGJLWEADerwkgjne jerngkjenrawiusdw wefje gkerjgodifsg ln</p>
+          <p>Amy Riley remembers the time someone wished her a happy Mother's Day and she broke down in tears, right in the middle of a Wegman's supermarket.
+
+"I found Mother's Day to be impossible," the Collingswood, New Jersey, woman said. She had been pregnant six times, but none of those pregnancies lasted. Motherhood, she worried, might never happen for her, and the realization and the reminders were painful.Amy Riley remembers the time someone wished her a happy Mother's Day and she broke down in tears, right in the middle of a Wegman's supermarket.
+
+"I found Mother's Day to be impossible," the Collingswood, New Jersey, woman said. She had been pregnant six times, but none of those pregnancies lasted. Motherhood, she worried, might never happen for her, and the realization and the reminders were painful.</p>
         </div>
 
         <footer class="name-and-date">
           <h5>Jane Schmidt, 10-24-24 2:50PM</h5>
         </footer>
+      </div> <!-- end blog-post-->
 
-        <div class="blog-post">
+
+      <div class="blog-post">
 
         <header class="subject">
           <h1>What to do if your dog wont stop barking</h1>
@@ -39,8 +52,9 @@
           <h5>Jane Schmidt, 10-24-24 2:50PM</h5>
         </footer>
 
-      </div>
-      </div>
-    </div>
+      </div><!-- end blog-post-->
+    </div><!-- end blog-->
+
+
   </body>
 </html>
