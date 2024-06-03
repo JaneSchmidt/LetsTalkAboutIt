87a597e53fbbdda42f629bba528d87dedae035a5
janeschmidt <jane.schmidt123@gmail.com>
2024-06-02 23:39:28 -0400

    navbar redesign

--- htdocs/Attributes/NavBar.php
+++ htdocs/Attributes/NavBar.php
@@ -6,18 +6,20 @@
 
   </head>
   <body>
-    <nav class="navbar">
+      <nav class="navbar">
+
         <div class="navdiv">
-            <div class="logo"><a href="/"><img src="../Attributes/logo.jpg" /></a></div>
-            <ul>
+          <div class="logo"><a href="/"><img src="../Attributes/logo.jpg" /></a></div>
+
+              <ul class="nav_links">
                 <li><a class="sign-in-button" href="login">SignIn</a></li>
                 <li><a class="sign-up-button" href="register">SignUp</a></li>
                 <?php if($_SESSION["user"] ?? false) : ?>
                   <li><a class="sign-out-button" href="logout">LogOut</a></li>
                   <li><p>profile</p></li>
                 <?php endif ?>
-            </ul>
+              </ul>
         </div>
-    </nav>
+      </nav>
   </body>
 </html>
